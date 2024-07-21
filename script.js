f7a3b6290f62798cfcba86ec119b1874f3736478
Moxx2 <moxxlearnin@Xubuntu.myguest.virtualbox.org>
2024-07-21 14:29:46 +0300

    Added button
    Added button 'Start' adnd wrote it's functionality. Etch-a-sketch is almost ready.

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const body = document.querySelector('body');
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 body.appendChild(container);
+const btn = document.querySelector('button');
+let userAnswer = '';
 
 function createRow() {
     const row = document.createElement('div');
     row.setAttribute('class', 'row');
     container.appendChild(row);
     
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < userAnswer; i++) {
         const square = document.createElement('div');
         square.setAttribute('class', 'square');
         square.addEventListener('mouseenter', () => {
@@ -19,5 +21,18 @@ function createRow() {
     
 };
 
-createRow();
-createRow();
+btn.addEventListener('click', () => {
+    userAnswer = prompt('Choose grid size (0-100)');
+    if (userAnswer > 100) {
+        alert('The grid is too big!')
+    } else {
+        container.querySelectorAll('*').forEach(n => n.remove());
+        let i = 0;
+        do {
+            i += 1; 
+            createRow();
+        } while (i < userAnswer);
+    };
+});
+
+
