52d7a3ff9ccab3c20cf9615aee6c4669f5f5ae11
Moxx2 <moxxlearnin@Xubuntu.myguest.virtualbox.org>
2024-07-21 11:43:29 +0300

    Added function createRow
    I reworked grid a little, now square will be generated only in one row.

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const container = document.createElement('div');
 container.setAttribute('class', 'container');
 body.appendChild(container);
 
-for (i = 0; i <=15; i++) {
-    const square = document.createElement('div');
-    square.setAttribute('class', 'square');
-    square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = 'red';
-    });
-    container.appendChild(square);
+function createRow() {
+    const row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    container.appendChild(row);
+    
+    for (i = 0; i < 5; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'red';
+        });
+        row.appendChild(square);
+    };
+    
 };
+
+createRow();
+createRow();
